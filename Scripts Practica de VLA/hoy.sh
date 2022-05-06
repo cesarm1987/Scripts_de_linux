84acba1d676bdf786750add373961e457d5c0a18
Cesar Alonso Morera Alpizar <cesarmorera@hotmail.com>
2022-05-05 22:36:00 -0600

    Add files via upload

--- Scripts Practica de VLA/hoy.sh
+++ Scripts Practica de VLA/hoy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
-fecha=`date | cut -d " " -f 1-3`
-hora=`date | cut -d " " -f 4`
+fecha=`date | cut -d " " -f 1-4`
+hora=`date | cut -d " " -f 5`
 echo "Hoy es $fecha y son las $hora"
 echo "Hoy es $fecha" > /home/hoy
 echo "Y son las $hora" >> /home/hoy
