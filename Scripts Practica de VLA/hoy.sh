b6b117b38f10c688c23a59503f529bf9f101d290
Cesar Alonso Morera Alpizar <cesarmorera@hotmail.com>
2024-09-21 13:53:49 -0600

    Delete Scripts Practica de VLA directory

--- Scripts Practica de VLA/hoy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-clear
-fecha=`date | cut -d " " -f 1-4`
-hora=`date | cut -d " " -f 5`
-echo "Hoy es $fecha y son las $hora"
-echo "Hoy es $fecha" > /home/hoy
-echo "Y son las $hora" >> /home/hoy
-exit
--- Scripts Practica de VLA/todo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-clear
-cat /home/hoy > /home/todo
-cat /home/mi_informacion >> /home/todo
-cat /home/mi_informacion2 >> /home/todo
-cat /home/mi_familia >> /home/todo
-cat /home/todo
-exit
