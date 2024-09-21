e1b5cae2451b26998369b37ad7836fc653920a97
Cesar Alonso Morera Alpizar <cesarmorera@hotmail.com>
2024-09-21 13:53:24 -0600

    Add files via upload

---
+++ Ejercicios/Script_Ejemplo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Este es un Script de Prueba"
+echo " " 
+date
+echo " "
+echo "Fin del Script"
---
+++ Ejercicios/Script_Variable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "El script lee e imprime el valor de la variable PRUEBA"
+echo " " 
+echo "El valor de la variable PRUEBA es:" $PRUEBA
+
+
