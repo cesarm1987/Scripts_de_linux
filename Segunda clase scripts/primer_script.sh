8bba095bf807a1cd8a92dc5a89b33455c6ab4e7a
Cesar Alonso Morera Alpizar <cesarmorera@hotmail.com>
2024-09-21 13:53:57 -0600

    Delete Segunda clase scripts directory

--- Segunda clase scripts/primer_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-clear
-echo "=============== Mi primer script ================="
-echo ""
-uptime
-free -m
-echo ""
-echo "Fin del Script"
--- Segunda clase scripts/script_while.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-c=1
-while [ $c -le 5 ]
-do
-   echo "Bienvenido $c veces"
-   (( c++ ))
-done
